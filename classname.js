8a4a749a216e5f93eebe952af50d362ba091e5bb
Sufian Rhazi <srhazi@etsy.com>
2015-10-28 12:24:08 -0400

    Factor out check from of inner loop
    This reduces complexity and increases readability

--- classname.js
+++ classname.js
@@ -1,15 +1,22 @@
+var hasClass = function (element, className) {
+    var isFound = false;
+    Array.prototype.forEach.call(childNode.classList, function (childNodeClassName) {
+        if (childNodeClassName === className) {
+            isFound = true;
+        }
+    });
+    return isFound;
+};
+
 var getElementsByClassName = function (className) {
     var results = [];
 
     function recursion(input) {
         for (var i = 0; i < input.childNodes.length; i++) {
             if (input.childNodes[i].nodeType === 1) {
-                var childNode = input.childNodes[i];
-                Array.prototype.forEach.call(childNode.classList, function (childNodeClassName) {
-                    if (childNodeClassName === className) {
-                        results.push(childNode);
-                    }
-                });
+                if (hasClass(input.childNodes[i])) {
+                    results.push(input.childNodes[i]);
+                }
                 recursion(input.childNodes[i]);
             }
         }
