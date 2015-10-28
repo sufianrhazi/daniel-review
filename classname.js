aca52a0d27149355135f867c9b09b78eb37c0116
Sufian Rhazi <srhazi@etsy.com>
2015-10-28 12:12:11 -0400

    initial solution

---
+++ classname.js
@@ -0,0 +1,26 @@
+var getElementsByClassName = function(className){
+   var allNodesList = [];
+   var positiveNodes = [];
+   var results = [];
+
+   function recursion (input){
+     if (input.childNodes.length > 0){
+       for (var i = 0; i < input.childNodes.length; i++) {
+         if (input.childNodes[i].nodeType === 1) {
+           allNodesList.push(input.childNodes[i]);
+           recursion(input.childNodes[i]);
+         };
+       };
+     };
+   }; 
+   recursion(document);
+
+   allNodesList.forEach(function(positiveNode){
+     positiveNode.classList.forEach(function(matchingNode){
+       if (matchingNode === className){
+          results.push(positiveNode);
+       }
+     })
+   }) 
+   return results
+};
