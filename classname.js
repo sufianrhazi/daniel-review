3abbbd52628e0442cbe5160518fab5512d7f5ab3
Sufian Rhazi <srhazi@etsy.com>
2015-10-28 12:36:49 -0400

    Nitpick
    Not really important, but sometimes you'll see `[].forEach` over
    `Array.prototype.forEach` -- it's concise and equivalent

--- classname.js
+++ classname.js
@@ -1,5 +1,5 @@
 var hasClass = function (element, className) {
-    return Array.prototype.some.call(childNode.classList, function (childNodeClassName) {
+    return [].some.call(childNode.classList, function (childNodeClassName) {
         return childNodeClassName === className;
     });
 };
@@ -8,7 +8,7 @@ var getElementsByClassName = function (className) {
     var results = [];
 
     function recursion(input) {
-        Array.prototype.forEach.call(input.childNodes, function (childNode) {
+        [].forEach.call(input.childNodes, function (childNode) {
             if (childNode.nodeType === 1 && hasClass(childNode)) {
                 results.push(childNode);
             }
